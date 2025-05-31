bae79494f018612f6568589c831d8386add65f46
AmirHosein <sh.amirhosein@gmail.com>
2025-06-01 01:00:23 +0330

    first-commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,61 @@
 import Image from "next/image";
+import Link from "next/link";
+
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    <>
+      <main>
+        <div className="h-fit rounded-b-2xl rounded-l-2xl p-10">
+          <div className="text-white vazirfont-medium text-sm">
+            خانه
+          </div>
+          <div className="grid grid-cols-12 flex items-center">
+            <div className="col-span-8">
+              <div className="vazirfont-bold text-lg flex gap-1 text-white">
+                <span>✔️</span>
+                <h1>
+                  ارایه سرورهای مجازی با کیفیت بالا و قیمت مناسب
+                </h1>
+              </div>
+              <div className="vazirfont-bold text-xl text-white mt-5 gap-1">
+                <span>✔️</span>
+                <span>انتخاب سرور مناسب، اولین قدم برای ایده های بزرگ</span>
+              </div>
+              <div className="vazirfont-bold text-3xl mt-5 text-white">
+                {/* <span>✔️</span> */}
+                <span>ما اینجاییم تا رویای دیجیتال شما به واقعیت تبدیل شود</span>
+              </div>
+              <div className="mt-10 flex">
+                <Link className="mx-auto" href="#services">
+                  <span className="bg-white hover:bg-gray-200 text-black vazirfont-bold pr-5 pl-5 pt-2 pb-2 rounded-3xl">✔️ شروع کنید</span>
+                </Link>
+              </div>
+            </div>
+            <div className="col-span-4 flex justify-center">
+              <Image src={"/img/vps.webp"} alt="vps" width={300} height={300} />
+            </div>
+          </div>
+        </div>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+        <div id="services" className="bg-white h-fit rounded-2xl  p-10 mt-10">
+          {/* <div className="flex justify-center text-3xl vazirfont-bold">
+            VPS Bazaar
+          </div> */}
+          <section className="">
+            <div className="flex justify-center w-full">
+              <h2 className="mx-auto vazirfont-bold text-3xl">
+                سرویس ها
+              </h2>
+            </div>
+            <div className="grid grid-cols-12">
+              <div className="">
+                <div>flex</div>
+              </div>
+            </div>
+          </section>
         </div>
       </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+    </>
   );
 }
